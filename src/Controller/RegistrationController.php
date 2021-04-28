a9bfbf7e3e8ae4bb78e3536c830658d1fffc5630
wilfried <wilpel90@gmail.com>
2021-04-28 07:19:07 +0200

    les interfaces

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -18,27 +18,27 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistrationController extends AbstractController
 {
-    /**
-     * @var Mailer
-     */
-    private $mailer;
+    // /**
+    //  * @var Mailer
+    //  */
+    // private $mailer;
 
-    /**
-     * @var UserRepository;
-     */
+    // /**
+    //  * @var UserRepository;
+    //  */
 
-    public function __construct(UserPasswordEncoderInterface $passwordEncoder, MailerInterface $mailer, UserRepository $userRepository)
-    {
-        $this->$passwordEncoder = $passwordEncoder;
-        $this->mailer = $mailer;
+    // public function __construct(UserPasswordEncoderInterface $passwordEncoder, MailerInterface $mailer, UserRepository $userRepository)
+    // {
+    //     $this->$passwordEncoder = $passwordEncoder;
+    //     $this->mailer = $mailer;
         
-    }
+    // }
     /**
      * @Route("/registration", name="register")
      * @param Request $request
      * @return Response
      */
-    public function register(Request $request, UserPasswordEncoderInterface $encoder): Response
+    public function register(Request $request): Response
 
     {
         $user = new User();
@@ -49,13 +49,13 @@ class RegistrationController extends AbstractController
 
         if($form->isSubmitted() && $form->isValid()) {
 
-            $user->setPassword($encoder->encodePassword($user, $form->get("password")->getData()));
+        //     $user->setPassword($encoder->encodePassword($user, $form->get("password")->getData()));
         
-        $user->setToken($this->generateToken());
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($user);
-        $em->flush();
-        $this->mailer->sendEmail($user->getEmail(), $user->getToken());
+        // $user->setToken($this->generateToken());
+        // $em = $this->getDoctrine()->getManager();
+        // $em->persist($user);
+        // $em->flush();
+        // $this->mailer->sendEmail($user->getEmail(), $user->getToken());
       
         }
 
@@ -63,39 +63,39 @@ class RegistrationController extends AbstractController
             'form' => $form->createView()
         ]);
     }
-    /**
-     * @Route("/confirmer-mon-compte/{token}", name="confirm_account")
-     * @param string $token
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
-     */
-    public function confirmAccount(string $token)
-    {
-        $user = $this->userRepository->findOneBy(["token" => $token]);
+//     /**
+//      * @Route("/confirmer-mon-compte/{token}", name="confirm_account")
+//      * @param string $token
+//      * @return \Symfony\Component\HttpFoundation\JsonResponse
+//      */
+//     public function confirmAccount(string $token)
+//     {
+//         $user = $this->userRepository->findOneBy(["token" => $token]);
 
-        if($user) {
+//         if($user) {
 
-        $user->setToken(null);
-        $user->setEnabled(true);
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($user);
-        $em->flush();
-        $this->addFlash("succes", "compte actif");
-        return $this->redirectToRoute("home");
+//         $user->setToken(null);
+//         $user->setEnabled(true);
+//         $em = $this->getDoctrine()->getManager();
+//         $em->persist($user);
+//         $em->flush();
+//         $this->addFlash("succes", "compte actif");
+//         return $this->redirectToRoute("home");
 
-        }else{
+//         }else{
 
-            $this->addFlash("error", "ce compte n'existe pas");
-            return $this->redirectToRoute('home');
+//             $this->addFlash("error", "ce compte n'existe pas");
+//             return $this->redirectToRoute('home');
 
-        }
+//         }
 
       
-    }
+//     }
 
 
-private function generateToken()
+// private function generateToken()
 
-{
-    return rtrim(strtr(base64_encode(random_bytes(32)), '+/','-_'), '=');
-}
+// {
+//     return rtrim(strtr(base64_encode(random_bytes(32)), '+/','-_'), '=');
+// }
 }
